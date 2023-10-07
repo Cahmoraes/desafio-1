fe56789b5f5a49256cac2b6a518865be5268554a
Caique Moraes <ike.moraes93@gmail.com>
2023-10-07 11:07:34 -0300

    test: extract variable

--- src/domain/usecases/parents/create-parent.usecase.spec.ts
+++ src/domain/usecases/parents/create-parent.usecase.spec.ts
@@ -1,10 +1,19 @@
 import { InMemoryParentsRepository } from '@/infra/repositories/in-memory/in-memory-parents.repository'
 import { CreateParentUseCase } from './create-parent.usecase'
 import { Phone } from '@/domain/entities/value-objects/phone'
+import { ParentProps } from '@/domain/entities/parent.entity'
 
 describe('Create Parent Use Case', () => {
   let sut: CreateParentUseCase
   let parentsRepository: InMemoryParentsRepository
+  const dummyParent: ParentProps = {
+    name: 'any_name',
+    lastName: 'any_sobrenome',
+    phones: ['0123456789', '1234567890'],
+    emails: ['any_email'],
+    address: ['any_address'],
+    cpf: 'any_cpf',
+  }
 
   beforeEach(() => {
     parentsRepository = new InMemoryParentsRepository()
@@ -12,15 +21,7 @@ describe('Create Parent Use Case', () => {
   })
 
   test('Deve criar um Parent', async () => {
-    await sut.execute({
-      name: 'any_name',
-      lastName: 'any_sobrenome',
-      phones: ['0123456789', '1234567890'],
-      emails: ['any_email'],
-      address: ['any_address'],
-      cpf: 'any_cpf',
-    })
-
+    await sut.execute(dummyParent)
     expect(parentsRepository.data.size).toBe(1)
     expect(parentsRepository.data.toArray()[0].name).toEqual('any_name')
     expect(parentsRepository.data.toArray()[0].lastName).toEqual(
