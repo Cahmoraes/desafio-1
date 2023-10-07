25a185d8bf52dfbd99fa69f83904c1dbba35f039
Caique Moraes <ike.moraes93@gmail.com>
2023-10-07 11:35:42 -0300

    feat: classrooms.repository.ts

---
+++ src/infra/repositories/in-memory/in-memory-classrooms.repository.ts
@@ -0,0 +1,36 @@
+import { ClassRoom } from '@/domain/entities/classroom.entity'
+import { ClassRoomsRepository } from '@/domain/repositories/classrooms.repository'
+import ExtendedSet from '@cahmoraes93/extended-set'
+
+export class InMemoryClassRoomsRepository implements ClassRoomsRepository {
+  public LIMIT_PER_PAGE = 20
+  public data = new ExtendedSet<ClassRoom>()
+
+  async save(aClassRoom: ClassRoom): Promise<void> {
+    this.data.add(aClassRoom)
+  }
+
+  async update(aClassRoom: ClassRoom): Promise<void> {
+    const classroomToDelete = this.data.find((classroom) =>
+      classroom.id.equals(aClassRoom.id),
+    )
+    if (classroomToDelete) this.data.delete(classroomToDelete)
+    this.data.add(aClassRoom)
+  }
+
+  async classroomOfId(aClassRoomId: string): Promise<ClassRoom | null> {
+    return this.data.find(
+      (classroom) => classroom.id.toString() === aClassRoomId,
+    )
+  }
+
+  async delete(aClassRoom: ClassRoom): Promise<void> {
+    this.data.delete(aClassRoom)
+  }
+
+  async allClassRooms(page: number): Promise<ClassRoom[]> {
+    return this.data
+      .toArray()
+      .slice((page - 1) * this.LIMIT_PER_PAGE, page * this.LIMIT_PER_PAGE)
+  }
+}
