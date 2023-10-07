88ca17ec343a8692d4b885714f2b01c5e362d6c2
Caique Moraes <ike.moraes93@gmail.com>
2023-10-07 11:09:27 -0300

    feat: delete-teacher.usecase.ts

---
+++ src/domain/usecases/teachers/delete-teacher.usecase.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryTeachersRepository } from '@/infra/repositories/in-memory/in-memory-teachers.repository'
+import { DeleteTeacherUseCase } from './delete-teacher.usecase'
+import { Teacher, TeacherProps } from '@/domain/entities/teacher.entity'
+
+describe('Delete Teacher Use Case', () => {
+  let sut: DeleteTeacherUseCase
+  let teachersRepository: InMemoryTeachersRepository
+  const dummyTeacher: TeacherProps = {
+    firstName: 'any_name',
+    lastName: 'any_lastName',
+    cpf: 'any_cpf',
+    phone: 'any_phone',
+    email: 'any_email',
+    hiringDate: new Date(),
+    wage: 1000,
+    specialization: 'any_specialization',
+  }
+
+  beforeEach(() => {
+    teachersRepository = new InMemoryTeachersRepository()
+    sut = new DeleteTeacherUseCase(teachersRepository)
+  })
+
+  test('Deve criar um Teacher', async () => {
+    const teacher = Teacher.create(dummyTeacher)
+    await teachersRepository.save(teacher)
+
+    await sut.execute({
+      teacherId: teacher.id.toString(),
+    })
+
+    expect(teachersRepository.data.size).toBe(0)
+  })
+
+  test('Deve gerar um erro ao tentar deletar um Teacher não existente', async () => {
+    await expect(() =>
+      sut.execute({ teacherId: 'any_id' }),
+    ).rejects.toThrowError('Teacher of id [any_id] not found')
+  })
+})
---
+++ src/domain/usecases/teachers/delete-teacher.usecase.ts
@@ -0,0 +1,19 @@
+import { TeachersRepository } from '@/domain/repositories/teachers.repository'
+
+interface DeleteTeacherUseCaseInput {
+  teacherId: string
+}
+
+type DeleteTeacherUseCaseOutput = void
+
+export class DeleteTeacherUseCase {
+  constructor(private readonly teachersRepository: TeachersRepository) {}
+
+  public async execute({
+    teacherId,
+  }: DeleteTeacherUseCaseInput): Promise<DeleteTeacherUseCaseOutput> {
+    const teacher = await this.teachersRepository.teacherOfId(teacherId)
+    if (!teacher) throw new Error(`Teacher of id [${teacherId}] not found`)
+    await this.teachersRepository.delete(teacher)
+  }
+}
