e95b6c788d84a3f74834aac8c826e62d87203c1d
Caique Moraes <ike.moraes93@gmail.com>
2023-10-07 11:42:12 -0300

    feat: delete-classroom.usecase.ts

---
+++ src/domain/usecases/classrooms/delete-classroom.usecase.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryClassRoomsRepository } from '@/infra/repositories/in-memory/in-memory-classrooms.repository'
+import { DeleteClassRoomUseCase } from './delete-classroom.usecase'
+import { ClassRoom, ClassRoomProps } from '@/domain/entities/classroom.entity'
+
+describe('Delete ClassRoom Use Case', () => {
+  let sut: DeleteClassRoomUseCase
+  let classroomsRepository: InMemoryClassRoomsRepository
+  const dummyClassRoom: ClassRoomProps = {
+    maxStudentsNumber: 1,
+    minAge: 1,
+    discipline: 'any_discipline',
+    duration: 1,
+  }
+
+  beforeEach(() => {
+    classroomsRepository = new InMemoryClassRoomsRepository()
+    sut = new DeleteClassRoomUseCase(classroomsRepository)
+  })
+
+  test('Deve criar um ClassRoom', async () => {
+    const classroom = ClassRoom.create(dummyClassRoom)
+    await classroomsRepository.save(classroom)
+
+    await sut.execute({
+      classroomId: classroom.id.toString(),
+    })
+
+    expect(classroomsRepository.data.size).toBe(0)
+  })
+
+  test('Deve gerar um erro ao tentar deletar um ClassRoom não existente', async () => {
+    await expect(() =>
+      sut.execute({ classroomId: 'any_id' }),
+    ).rejects.toThrowError('ClassRoom of id [any_id] not found')
+  })
+})
---
+++ src/domain/usecases/classrooms/delete-classroom.usecase.ts
@@ -0,0 +1,21 @@
+import { ClassRoomsRepository } from '@/domain/repositories/classrooms.repository'
+
+interface DeleteClassRoomUseCaseInput {
+  classroomId: string
+}
+
+type DeleteClassRoomUseCaseOutput = void
+
+export class DeleteClassRoomUseCase {
+  constructor(private readonly classroomsRepository: ClassRoomsRepository) {}
+
+  public async execute({
+    classroomId,
+  }: DeleteClassRoomUseCaseInput): Promise<DeleteClassRoomUseCaseOutput> {
+    const classroom = await this.classroomsRepository.classroomOfId(classroomId)
+    if (!classroom) {
+      throw new Error(`ClassRoom of id [${classroomId}] not found`)
+    }
+    await this.classroomsRepository.delete(classroom)
+  }
+}
