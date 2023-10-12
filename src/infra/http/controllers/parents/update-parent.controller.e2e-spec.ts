27e8d0db5c13d16b0477b4ef343ccabecca12210
Caique Moraes <ike.moraes93@gmail.com>
2023-10-12 00:24:33 -0300

    feat: inversify

---
+++ src/infra/http/controllers/parents/update-parent.controller.e2e-spec.ts
@@ -0,0 +1,76 @@
+import getPort from 'get-port'
+import request from 'supertest'
+import { MainHttpController } from '../main-http.controller'
+import { ParentUseCaseFactory } from '@/application/usecases/parents/factories/parent-usecase.factory'
+import { ParentPresenter } from '@/infra/presenters/parent.presenter'
+import { FastifyAdapter } from '../../server/fastify-adapter'
+import { Parent, ParentProps } from '@/domain/entities/parent.entity'
+import { FSParentsRepository } from '@/infra/repositories/file-system/fs-parents.respitory'
+import { TestingFSDatabase } from '@/infra/repositories/file-system/testing-fs-database'
+import { ParentsRoutes } from './parents-routes.enum'
+import { makeParamWithId } from '@/tests/utils/make-param-with-id'
+import { Name } from '@/domain/entities/value-objects/name'
+
+describe('Create Parent (e2e)', () => {
+  let fastify: FastifyAdapter
+  let testingFSDatabase: TestingFSDatabase
+  const dummyParent: ParentProps = {
+    name: 'any_name',
+    lastName: 'any_sobrenome',
+    phones: ['0123456789', '1234567890'],
+    emails: ['any_email'],
+    address: ['any_address'],
+    cpf: 'any_cpf',
+  }
+
+  beforeAll(async () => {
+    const port = await getPort()
+    fastify = new FastifyAdapter({ port })
+    testingFSDatabase = new TestingFSDatabase()
+    const parentsRepository = new FSParentsRepository(testingFSDatabase)
+    const parentPresenter = new ParentPresenter()
+    const parentUseCaseFactory = new ParentUseCaseFactory(
+      parentsRepository,
+      parentPresenter,
+    )
+    const mainHttpController = new MainHttpController(
+      fastify,
+      parentUseCaseFactory,
+    )
+
+    mainHttpController.init()
+    await fastify.listen()
+  })
+
+  afterAll(async () => {
+    await fastify.close()
+  })
+
+  test('Deve atualizar um Parent', async () => {
+    const createResponse = await request(fastify.server)
+      .post(ParentsRoutes.CREATE)
+      .send(dummyParent)
+
+    const { id } = createResponse.body
+    expect(createResponse.statusCode).toBe(200)
+
+    const updateResponse = await request(fastify.server)
+      .put(makeParamWithId(ParentsRoutes.PUT, id))
+      .send({
+        name: 'change_name',
+        lastName: 'change_sobrenome',
+        phones: ['111111111', '000000000'],
+        emails: ['any_email'],
+        address: ['change_address'],
+      })
+
+    expect(updateResponse.statusCode).toBe(200)
+
+    const responseGetParent = await request(fastify.server).get(
+      makeParamWithId(ParentsRoutes.GET, id),
+    )
+
+    console.log(responseGetParent.body)
+    expect(responseGetParent.statusCode).toBe(200)
+  })
+})
---
+++ src/infra/http/controllers/parents/update-parent.controller.ts
@@ -0,0 +1,22 @@
+import { ParentUseCaseFactory } from '@/application/usecases/parents/factories/parent-usecase.factory'
+import { HandlerParams } from '../../server/handler-params/handler-params'
+
+export class UpdateParentController {
+  constructor(private readonly parentUseCaseFactory: ParentUseCaseFactory) {
+    this.bindMethod()
+  }
+
+  private bindMethod() {
+    this.handleRequest = this.handleRequest.bind(this)
+  }
+
+  public async handleRequest({ request }: HandlerParams): Promise<void> {
+    const updateUseCase = this.parentUseCaseFactory.createUpdateParentUseCase()
+    const body = request.body as any
+    const params = request.params as any
+    return updateUseCase.execute({
+      parentId: params.parentId,
+      fields: body,
+    })
+  }
+}
