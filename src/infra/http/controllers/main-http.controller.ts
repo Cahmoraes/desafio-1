f4ec99597081203c4a92087ba70e27478ccfd22f
Caique Moraes <ike.moraes93@gmail.com>
2023-10-20 18:35:28 -0300

    feat: parents.controller.ts

--- src/infra/http/controllers/main-http.controller.ts
+++ src/infra/http/controllers/main-http.controller.ts
@@ -1,11 +1,6 @@
-import { HTTPMethodTypes, HttpServer } from '../server/http-server'
+import { HttpServer } from '../server/http-server'
 import { ParentUseCaseFactory } from '@/application/usecases/parents/factories/parent-usecase.factory'
-import { CreateParentController } from './parents/create-parent.controller'
-import { GetParentController } from './parents/get-parent.controller'
-import { ParentsRoutes } from './parents/parents-routes.enum'
-import { DeleteParentController } from './parents/delete-parent.controller'
-import { UpdateParentController } from './parents/update-parent.controller'
-import { FetchParentsController } from './parents/fetch-parents.controller'
+import { ParentsController } from './parents.controller'
 
 export class MainHttpController {
   constructor(
@@ -14,50 +9,10 @@ export class MainHttpController {
   ) {}
 
   public init(): void {
-    this.registerCreateParentController()
-    this.registerGetParentController()
-    this.registerDeleteParentController()
-    this.registerUpdateParentController()
-    this.registerFetchParentsController()
+    this.registerParentsController()
   }
 
-  private registerCreateParentController(): void {
-    this.httpServer.on(
-      HTTPMethodTypes.POST,
-      ParentsRoutes.CREATE,
-      new CreateParentController(this.parentUseCaseFactory).handleRequest,
-    )
-  }
-
-  private registerGetParentController(): void {
-    this.httpServer.on(
-      HTTPMethodTypes.GET,
-      ParentsRoutes.GET,
-      new GetParentController(this.parentUseCaseFactory).handleRequest,
-    )
-  }
-
-  private registerDeleteParentController(): void {
-    this.httpServer.on(
-      HTTPMethodTypes.DELETE,
-      ParentsRoutes.DELETE,
-      new DeleteParentController(this.parentUseCaseFactory).handleRequest,
-    )
-  }
-
-  private registerUpdateParentController(): void {
-    this.httpServer.on(
-      HTTPMethodTypes.PUT,
-      ParentsRoutes.PUT,
-      new UpdateParentController(this.parentUseCaseFactory).handleRequest,
-    )
-  }
-
-  private registerFetchParentsController(): void {
-    this.httpServer.on(
-      HTTPMethodTypes.GET,
-      ParentsRoutes.FETCH,
-      new FetchParentsController(this.parentUseCaseFactory).handleRequest,
-    )
+  private registerParentsController(): void {
+    new ParentsController(this.httpServer, this.parentUseCaseFactory).init()
   }
 }
---
+++ src/infra/http/controllers/parents.controller.ts
@@ -0,0 +1,63 @@
+import { ParentUseCaseFactory } from '@/application/usecases/parents/factories/parent-usecase.factory'
+import { HTTPMethodTypes, HttpServer } from '../server/http-server'
+import { ParentsRoutes } from './parents/parents-routes.enum'
+import { CreateParentController } from './parents/create-parent.controller'
+import { DeleteParentController } from './parents/delete-parent.controller'
+import { FetchParentsController } from './parents/fetch-parents.controller'
+import { GetParentController } from './parents/get-parent.controller'
+import { UpdateParentController } from './parents/update-parent.controller'
+
+export class ParentsController {
+  constructor(
+    private readonly httpServer: HttpServer,
+    private readonly parentUseCaseFactory: ParentUseCaseFactory,
+  ) {}
+
+  public init(): void {
+    this.registerCreateParentController()
+    this.registerGetParentController()
+    this.registerDeleteParentController()
+    this.registerUpdateParentController()
+    this.registerFetchParentsController()
+  }
+
+  private registerCreateParentController(): void {
+    this.httpServer.on(
+      HTTPMethodTypes.POST,
+      ParentsRoutes.CREATE,
+      new CreateParentController(this.parentUseCaseFactory).handleRequest,
+    )
+  }
+
+  private registerGetParentController(): void {
+    this.httpServer.on(
+      HTTPMethodTypes.GET,
+      ParentsRoutes.GET,
+      new GetParentController(this.parentUseCaseFactory).handleRequest,
+    )
+  }
+
+  private registerDeleteParentController(): void {
+    this.httpServer.on(
+      HTTPMethodTypes.DELETE,
+      ParentsRoutes.DELETE,
+      new DeleteParentController(this.parentUseCaseFactory).handleRequest,
+    )
+  }
+
+  private registerUpdateParentController(): void {
+    this.httpServer.on(
+      HTTPMethodTypes.PUT,
+      ParentsRoutes.PUT,
+      new UpdateParentController(this.parentUseCaseFactory).handleRequest,
+    )
+  }
+
+  private registerFetchParentsController(): void {
+    this.httpServer.on(
+      HTTPMethodTypes.GET,
+      ParentsRoutes.FETCH,
+      new FetchParentsController(this.parentUseCaseFactory).handleRequest,
+    )
+  }
+}
